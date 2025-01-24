0653f1d4caebdbacf6bc250427272efa8c6d20c2
shariorfarhan07 <shariorfarhan77@gmail.com>
2025-01-24 15:13:26 +0600

    added in memory user hard coded

--- src/main/java/com/springsecurity/SpringSecEx/config/SecurityConfig.java
+++ src/main/java/com/springsecurity/SpringSecEx/config/SecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -19,13 +22,20 @@ public class SecurityConfig{
                 .authorizeHttpRequests(authorize ->{
                     authorize.requestMatchers("/").permitAll();
                     authorize.anyRequest().authenticated();
-                        }
-
-
-
-                )
-                 .formLogin(withDefaults())
+                })
+                .formLogin(withDefaults())
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                        .username("user")
+                        .password("{noop}pass") // {noop} is used for password encoder to understand that needs no encoding
+                        .authorities("USER")
+                        .build()
+        );
     }
 
 
